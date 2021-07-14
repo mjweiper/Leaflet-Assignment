0955d6f527ac24ac74cf12461b6b666c1e491b2f
Matthew Weiper <mattweiper@gmail.com>
2021-07-14 16:05:57 -0400

    edited scale function

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,6 @@
-    function createMap(earthQuakes) {
+{   
+   
+   function createMap(earthQuakes) {
     
         // Create the tile layer that will be the background of our map
         var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -21,7 +23,7 @@
         // Create the map object with options
         var map = L.map("map", {
             center: [19.8097343, -98.5556199],
-            zoom: 2.5,
+            zoom: 3,
             layers: [lightmap, earthQuakes]
         })
 
@@ -29,6 +31,7 @@
         L.control.layers(baseMaps, overlayMaps, {
             collapsed: false
         }).addTo(map);
+
 
     }
 
@@ -46,28 +49,42 @@
         // Loop through the array
         for (var index = 0; index < locations.length; index++) {
             var location = locations[index];
+            var r = location.properties.mag
+            console.log(r)
+                //scale for colors of magnitude
+                function getColor(r) {
+                    return r >= 6 ? '#B22222' :
+                    r >= 5 ? '#FF0000' :
+                    r >= 4 ? '#FF8C00' :
+                    r >= 3 ? '#FFA500' :
+                    r >= 2 ? '#FFFF00' :
+                    '#32CD32';
+                }
             
-            //console.log(markers)
-            var quakeMarker = L.circleMarker([location.geometry.coordinates[1], location.geometry.coordinates[0]])
-            //console.log(quakeMarker)
+            //create markers
+            var quakeMarker = L.circleMarker([location.geometry.coordinates[1], location.geometry.coordinates[0]],{radius: location.properties.mag * 2, color: getColor(location.properties.mag), fillcolor: getColor(location.properties.mag), fillOpacity: .7})
+            console.log(quakeMarker)
+
             quakeMarkers.push(quakeMarker);
+
             //Popup with more information
             quakeMarker.bindPopup("Information: " + location.properties.title);
-        
+            
         }
+            
+
+
         
-
-
     createMap(L.layerGroup(quakeMarkers));
 
     }
 
-  
+ 
+ 
 
     //EARTHQUAKE URL
     d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson').then(createMarkers);
 
 
 
-
-    
+}   
